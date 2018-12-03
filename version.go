f7288bcc90b8e38d16cf2549ead5b01d4400cf31
Cheyne Womble <cwomble@arista.com>
2018-12-03 13:47:32 -0500

    Update version description

--- version.go
+++ version.go
@@ -28,6 +28,9 @@
 // OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 // IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
+
+// Package cvprac provides client and api for Cloud Vision Portal
 package cvprac
 
+// Version of library
 const Version string = "v2.1.1"
